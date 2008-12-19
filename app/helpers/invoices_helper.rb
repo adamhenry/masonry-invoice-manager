3dc79dbd0eefe37e35c7ad328aa82b046f94e9fa
Adam Henry <a.david.henry@gmail.com>
2008-12-18 20:16:53 -0800

    yet another radom commit

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -8,7 +8,7 @@ module InvoicesHelper
 	end
   
   def render_action_button tag, obj
-      render :partial => "action_button", :locals => { :tag => tag, :obj => obj }
+      render :partial => "invoices/action_button", :locals => { :tag => tag, :obj => obj }
   end
 
   def render_link_to_confirm tag, method, obj
@@ -16,10 +16,7 @@ module InvoicesHelper
   end
 
   def render_invoice_link_to_confirm obj
-    #render_link_to_confirm 'Invoice', :get, obj if obj.can_invoice?
-    link_to 'Invoice',
-      '/invoices/invoice/' + obj.id.to_s,
-      :confirm => "Are you sure?\n You will no longer be able to Change Items on the invoice." if obj.can_invoice?
+    link_to 'Invoice', '/invoices/invoice/' + obj.id.to_s if obj.can_invoice?
   end
 
   def render_delete_link_to_confirm obj

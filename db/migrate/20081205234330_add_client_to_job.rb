c8a666d1b383b84e5c190c844cee37dc473a5b89
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-05 18:41:58 +0800

    Delete db directory

--- db/migrate/20081205234330_add_client_to_job.rb
+++
@@ -1,9 +0,0 @@
-class AddClientToJob < ActiveRecord::Migration
-  def self.up
-    add_column :jobs, :client, :string
-  end
-
-  def self.down
-    remove_column :jobs, :client
-  end
-end
--- db/migrate/20081209064515_add_date_to_payment.rb
+++
@@ -1,9 +0,0 @@
-class AddDateToPayment < ActiveRecord::Migration
-  def self.up
-    add_column :payments, :date, :date
-  end
-
-  def self.down
-    remove_column :payments, :date
-  end
-end

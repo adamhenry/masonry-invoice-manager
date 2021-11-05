5049e78a1d81cf63cbcfa4da9d27c2326a68eec8
removedporn <86824510+removedporn@users.noreply.github.com>
2021-11-05 18:41:25 +0800

    Delete app directory

--- app/helpers/payments_helper.rb
+++
@@ -1,5 +0,0 @@
-module PaymentsHelper
-  def payment_types
-    [ ["Select Payment type", "" ], [ "Check", "check"], [ "Credit", "credit" ] ]
-  end
-end
--- app/models/job.rb
+++
@@ -1,8 +0,0 @@
-class Job < ActiveRecord::Base
-	has_many :invoices, :dependent => :destroy
-
-  def can_delete?
-    true
-  end
-
-end
